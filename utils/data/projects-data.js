aefef53161d59a34597b5471ebc7ea576dbd1623
Noman <nmazhar911@gmail.com>
2024-11-08 12:01:35 +0500

    added projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,7 +1,8 @@
-import ayla from '/public/image/ayla.jpg';
+import configpilot from '/public/image/configpilot.png';
 import colabcube from '/public/image/colabcube.png';
-import realEstate from '/public/image/real-estate.jpg';
-import travel from '/public/image/travel.jpg';
+import green_pulse from '/public/image/green_pulse.png';
+import compassai from '/public/image/compassai.png';
+import { config } from 'next/dist/build/templates/pages';
 
 export const projectsData = [
     {
@@ -18,33 +19,39 @@ export const projectsData = [
     },
     {
         id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
-        code: '',
-        demo: '',
-        image: travel,
+        hackathon_name: 'Build Your AI Startup #2: Edge Runners [lablab.ai]',
+        hackathon_submission_link: 'https://lablab.ai/event/next2-edge-runners/compass-ai/compass-ai',
+        name: 'Compass AI',
+        description: 'Compass AI is a cutting-edge application designed to automate custom marketing strategies, adapting to the unique needs of brands. As the GenAI Engineer, I developed AI-driven content generation features that produce engaging and personalized content with minimal user input. The application simplifies social media publishing, enabling brands to maintain a strong online presence while focusing on growth.',
+        tools: ['Python', 'Streamlit', "OpenAI", "LLM", "Pillow", "Reportlab", "requests", "Tokenization", "Marekting Strategies"],
+        role: 'AI Developer',
+        code: 'https://github.com/chekosworld/compass-ai/',
+        demo: 'https://compass-ai.streamlit.app/',
+        image: compassai,
     },
     {
         id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+        hackathon_name: 'Tell Us a Climate Story [2024 NASA Space Apps Challenge]',
+        hackathon_submission_link: 'https://www.spaceappschallenge.org/nasa-space-apps-2024/find-a-team/green-pulse-nasa-hackathon/',
+        name: 'Tales from the Atmosphere',
+        description: 'Led a team to develop "Tales from the Atmosphere," a project that visualizes CO₂ emissions from various countries to enhance climate change awareness. Using Python and Pandas for data processing, we crafted interactive graphs and maps with Plotly, allowing users to explore emissions data in an engaging format. Additionally, I built a user-friendly website with WordPress and Elementor to present this data in an accessible way, aiming to promote climate education.',
+        tools: ['Python', 'Pandas', 'Plotly', 'Wordpress', 'Elementor'],
         code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
+        role: 'Team Lead || Data Analyst',
+        demo: 'https://earth-green-pulse.devlotech.com/',
+        image: green_pulse,
     },
     {
         id: 4,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
-        code: '',
-        demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
+        hackathon_name: 'Edge Runners 3.2 [lablab.ai]',
+        hackathon_submission_link: 'https://lablab.ai/event/edge-runners-3-point-2/edge-innovators/configpilot',
+        name: 'Edge Runners 3.2 [lablab.ai]',
+        description: "Built ConfigPilot, an AI-powered tool that automates network configurations for routers, switches, and servers, improving setup efficiency and reducing manual workload. Leveraging LLaMA 3.2, ConfigPilot delivers rapid, accurate responses for network configuration tasks, enhancing user experience. The responsive application was developed with React for a dynamic front-end and Streamlit for a streamlined, accessible site, catering to both IT professionals and non-technical users.",
+        tools: ['LLAMA 3.2', 'React', 'Streamlit', 'Python'],
+        code: 'https://github.com/nomanmazharr/config-pilot',
+        demo: 'https://configpilot.streamlit.app/',
+        image: configpilot,
+        role: 'GenAI Engineer',
     }
 ];
 
