55aaa2cc9834a19f9890d34afedc006517eb88a8
Noman <nmazhar911@gmail.com>
2024-11-10 15:49:08 +0500

    updating personal projects

---
+++ utils/data/personal-projects.js
@@ -0,0 +1,55 @@
+const projects = [
+    {
+        cover_image: '/image/colabcube.png',
+        title: 'ColabCube',
+        description: 'Virtual Co-Working Platform with collaborative tools and Blockchain-enabled Payments',
+        link: 'https://colabcubemainn.vercel.app/',
+        github: 'https://github.com/r7projects-shayan/colabcube-official/',
+    },
+    {
+        cover_image: '/image/compassai.png',
+        title: 'CompassAI',
+        description: 'Provides Automated Custom Marketing Strategies Tailored to Brand Needs',
+        link: 'https://compass-ai.streamlit.app/',
+        github: 'https://github.com/chekosworld/compass-ai/',
+    },
+    {
+        cover_image: '/image/configpilot.jpg',
+        title: 'ConfigPilot',
+        description: 'Provide Automated Network Device Configurations for Routers, Switches, and Servers.',
+        link: 'https://configpilot.streamlit.app/',
+        github: 'https://github.com/nomanmazharr/config-pilot',
+    },
+    {
+        cover_image: '/image/study-savvy.png',
+        title: 'Study Savvy',
+        description: 'Empowers Users to Learn more Effectively by Providing Personalized Study Plans',
+        link: 'https://study-savvy.streamlit.app/',
+        github: 'https://github.com/nomanmazharr/study-savvy',
+    },
+    {
+        cover_image: '/image/idea_crafter.jpg', 
+        title: 'Idea Crafter',
+        description: 'Application that Simplifies the Journey from Idea Generation to Realization',
+        link: 'https://idea-crafterr.streamlit.app/',
+        github: 'https://github.com/umairazmat/Idea-Crafter',
+    },
+    {
+        cover_image: '/image/rescure.jpg',
+        title: 'Rescure',
+        description: 'Provides AI-driven first-aid guidance during emergencies.',
+        link: 'https://drive.google.com/drive/u/0/folders/1d61EcRMbIxKAMizpogaup8fftPiGaTdq',
+        github: 'https://lablab.ai/event/aria-multimodal-hackathon/teamx/rescure',
+    },
+    {
+        cover_image: '/image/nasa.jpg',
+        title: 'Tell Us a Climate Story',
+        description: 'Provides Visualization on CO₂ emissions from Various Countries.',
+        link: 'https://earth-green-pulse.devlotech.com/',
+        github: 'https://www.spaceappschallenge.org/nasa-space-apps-2024/find-a-team/green-pulse-nasa-hackathon/',
+    },
+    // Add more projects here
+  ];
+  
+  export default projects;
+  
