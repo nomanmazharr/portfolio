899e9e431d9094577383be544f7ed329aa3fcbda
Noman <nmazhar911@gmail.com>
2024-11-11 06:43:08 +0500

    updating blog-card according to personla projects

--- app/components/homepage/blog/blog-card.jsx
+++ app/components/homepage/blog/blog-card.jsx
@@ -1,61 +1,45 @@
 // @flow strict
-import { timeConverter } from '@/utils/time-converter';
+
 import Image from 'next/image';
 import Link from 'next/link';
-import { BsHeartFill } from 'react-icons/bs';
-import { FaCommentAlt } from 'react-icons/fa';
 
-function BlogCard({ blog }) {
-
+function ProjectCard({ project }) {
   return (
-    <div className="border border-[#1d293a] hover:border-[#464c6a] transition-all duration-500 bg-[#1b203e] rounded-lg relative group"
-    >
+    <div className="border border-[#1d293a] hover:border-[#464c6a] transition-all duration-500 bg-[#1b203e] rounded-lg relative group">
       <div className="h-44 lg:h-52 w-auto cursor-pointer overflow-hidden rounded-t-lg">
         <Image
-          src={blog?.cover_image}
+          src={project.cover_image}
           height={1080}
           width={1920}
-          alt=""
-          className='h-full w-full group-hover:scale-110 transition-all duration-300'
+          alt={project.title}
+          className="h-full w-full group-hover:scale-110 transition-all duration-300"
         />
       </div>
       <div className="p-2 sm:p-3 flex flex-col">
-        <div className="flex justify-between items-center text-[#16f2b3] text-sm">
-          <p>{timeConverter(blog.published_at)}</p>
-          <div className="flex items-center gap-3">
-            <p className="flex items-center gap-1">
-              <BsHeartFill />
-              <span>{blog.public_reactions_count}</span>
+        <div className="flex justify-between items-center">
+          <Link href={project.link}>
+            <p className="my-2 lg:my-3 cursor-pointer text-lg text-white sm:text-xl font-medium hover:text-violet-500">
+              {project.title}
             </p>
-            {blog.comments_count > 0 &&
-              <p className="flex items-center gap-1">
-                <FaCommentAlt />
-                <span>{blog.comments_count}</span>
-              </p>
-            }
-          </div>
-        </div>
-        <Link target='_blank' href={blog.url}>
-          <p className='my-2 lg:my-3 cursor-pointer text-lg text-white sm:text-xl font-medium hover:text-violet-500'>
-            {blog.title}
-          </p>
-        </Link>
-        <p className='mb-2 text-sm text-[#16f2b3]'>
-          {`${blog.reading_time_minutes} Min Read`}
-        </p>
-        <p className='text-sm lg:text-base text-[#d3d8e8] pb-3 lg:pb-6 line-clamp-3'>
-          {blog.description}
-        </p>
-        {/* <div className="">
-          <Link target='_blank' href={blog.url}>
-            <button className='bg-violet-500 text-white px-3 py-1.5 rounded-full text-xs'>
-              Read More
-            </button>
           </Link>
-        </div> */}
+
+          {project.github && (
+            <a
+              href={project.github}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-4 py-2 text-sm text-white bg-gradient-to-r from-pink-500 to-violet-600 rounded-full hover:bg-violet-700 transition duration-300"
+            >
+              Code
+            </a>
+          )}
+        </div>
+        <p className="text-sm lg:text-base text-[#d3d8e8] pb-3 lg:pb-6 line-clamp-3">
+          {project.description}
+        </p>
       </div>
     </div>
   );
-};
+}
 
-export default BlogCard;
+export default ProjectCard;
